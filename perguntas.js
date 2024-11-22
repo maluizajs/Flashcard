ff8a4ae136b17e13dd7a230bf294bbc45a000665
maluizajs <00001111952711SP@al.educacao.sp.gov.br>
2024-11-22 16:19:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+    'Geografia', 
+'Qual a capital da Russia?',
+    'Moscou'
+)
+
+criaCartao(
+    'Matemática', 
+    'Qual a raiz quadrada de 25?',
+    '5'
+)
+
+criaCartao(
+    'Quimica', 
+    'Qual o nome do elemento H2O?',
+    'Água'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o valor π?',
+    '3,14'
+)
+
+criaCartao(
+    'História',
+    'Quais eram os países envolvidos na Guerra Fria?',
+    'União Soviética e Estados Unidos '
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual o coletivo de peixes?',
+    'Cardume'
+)
+
+criaCartao(
+    'Geográfia',
+    'Qual a capital da Itália?',
+    'Roma'
+)
+
+criaCartao(
+    'Biologia',
+    'Do que se alimentam os herbivoros?',
+    'Plantas'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual o coletivo de lobos?',
+    'Alcatéia'
+)
+
+
